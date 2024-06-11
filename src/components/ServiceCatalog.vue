6ac4f49e44445c944780ae4b08d08628f5ba8309
Sidhanth surana <sidhanth.surana12@gmail.com>
2024-06-11 16:37:42 +0530

    working on component

--- src/components/ServiceCatalog.vue
+++ src/components/ServiceCatalog.vue
@@ -1,28 +1,22 @@
 <template>
   <div class="service-catalog">
-    <h1>Service Catalog</h1>
-    <input
-      v-model="searchQuery"
-      class="search-input"
-      data-testid="search-input"
-      placeholder="Search services"
-    />
+    <div class="service-catalog__header">
+      <h1>Service Catalog</h1>
+      <input
+        v-model="searchQuery"
+        class="search-input"
+        data-testid="search-input"
+        placeholder="Search services"
+      />
+    </div>
 
     <div v-if="chunkedServices[currentPage]?.length">
-      <ul class="catalog">
-        <li
+      <div class="service-catalog__cards-container">
+        <ServiceCatalogProduct
           v-for="service in chunkedServices[currentPage]"
           :key="service.id"
-          class="service"
-        >
-          <div>
-            <p>
-              {{ service.name }}
-            </p>
-            <p>{{ service.description }}</p>
-          </div>
-        </li>
-      </ul>
+        />
+      </div>
 
       <!-- Buttons for going next and previos-->
       <div>
@@ -55,10 +49,13 @@
 <script lang="ts">
 import { defineComponent, ref, computed, watch, watchEffect } from "vue";
 import useServices from "@/composables/useServices";
-import type { R } from "vitest/dist/reporters-1evA5lom.js";
+import ServiceCatalogProduct from "@/components/ServiceCatalogProduct.vue";
 
 export default defineComponent({
   name: "ServiceCatalog",
+  components: {
+    ServiceCatalogProduct,
+  },
   setup() {
     // Import services from the composable
     const {
@@ -107,8 +104,9 @@ export default defineComponent({
 <style lang="scss" scoped>
 .service-catalog {
   margin: 2rem auto;
-  max-width: 1366px;
   padding: 0 20px;
+  background-color: lightgray;
+  max-width: 1366px;
 }
 
 .catalog {
@@ -137,5 +135,29 @@ export default defineComponent({
 
 input {
   padding: 8px 4px;
+}
+
+.service-catalog__header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.service-catalog__cards-container {
+  // show 9 products per page
+  // 3 products per row
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 0.5rem;
+
+  // for tablet view
+  @media (min-width: 768px) and (max-width: 1024px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  // for mobile view
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
---
+++ src/components/ServiceCatalogProduct.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="service-catalog-product__card">Card</div>
+</template>
+
+<style lang="scss" scoped>
+.service-catalog-product__card {
+  background-color: white;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  margin: 1rem;
+  padding: 1.6rem;
+  width: 28rem;
+  height: 14.5rem;
+}
+</style>
